7738e7c6b2dbe9f296e136dd9bcb82d2366516d1
HungHoDuy <hungduyhoqaz@gmail.com>
2023-10-19 17:56:21 +0700

    poster link

--- poster_link.py
+++ poster_link.py
@@ -2,7 +2,7 @@ import requests # the lib that handles the url stuff
 import json
 
 def poster_find(movie_id):
-    movie_id=19
+    movie_id='bda6d3e8'
     target_ur = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=c2c3992b8ca4966ce0e58aa69429fafe"
     resp = requests.get(target_ur)
     if resp.status_code == 200:
