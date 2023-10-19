5f623c55db358ceea67914102ee0f2a45d94f16b
Chunporo <hackingoxo@gmail.com>
2023-10-19 17:59:28 +0700

    poster call

--- posterFind.py
+++ posterFind.py
@@ -1,10 +1,10 @@
 import omdb
 
-
 def posterPathFind(imdb_id):
     # df['imdb_id'].head()
-    keyomdb = "bda6d3e8"
+    keyomdb = "720d0b09"
+    #keyomdb = "bda6d3e8"
     omdb.set_default("apikey", keyomdb)
     poster_json = omdb.imdbid(imdb_id)['poster']
-    # print(poster_json)
     return poster_json
+    # print(poster_json)
