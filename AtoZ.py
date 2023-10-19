f162ea35f2e4a298f211cf3c5a8e98eebd09fe2a
Chunporo <hackingoxo@gmail.com>
2023-10-19 16:57:35 +0700

    Add file

---
+++ AtoZ.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from data_filter import *
+
+
+def AtoZ_df(keyword):
+    re_list = DataFilter(keyword)
+    df = re_list[0]
+    # print(df)
+    keyword = ' '.join(re_list[1])
+    df = df.sort_values(by="title", ascending=True)
+    return df
+
+AtoZ_df('irod mad').to_csv('result_sample.csv')
