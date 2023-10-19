1429c561a9640d29db4706781a276048f7f406a4
HungHoDuy <hungduyhoqaz@gmail.com>
2023-10-19 16:56:57 +0700

    bugs fixed

--- data_filter.py
+++ data_filter.py
@@ -29,6 +29,7 @@ def Spell_fix(input):
     else:
         input = input.split()
         return input
+    print(spelling_fix)
     return spelling_fix
 
 def data_process():
@@ -99,6 +100,7 @@ def DataFilter(User_input,spell_check =True):
         corrected_keyword = Spell_fix(" ".join(keyword))
         if corrected_keyword != keyword:
             print (f"Showing result for {corrected_keyword}")
+        keyword = corrected_keyword
     tags = extract_tags_and_keywords(User_input)[0]
     Movie_list = title_search(df, keyword)
     Movie_list = tag_search(Movie_list, tags, genres_tags, language_tags,production_countries_tags,collection_tags, production_companies_tags)
@@ -109,8 +111,8 @@ def DataFilter(User_input,spell_check =True):
 
 # start_time = time.time()
 
-# result = DataFilter("immpossible mission")
-# print(result[["title", "original_title", "keywords"]])
+result = DataFilter("immpossible mission")
+print(result[["title", "original_title", "keywords"]])
 
 # end_time = time.time()
 # execution_time = end_time - start_time
