54603b218d337c689a52009f24d57eaaa42bfe19
Chunporo <hackingoxo@gmail.com>
2023-10-20 02:17:27 +0700

    relevant

--- relevant.py
+++ relevant.py
@@ -1,16 +1,20 @@
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 from data_filter import DataFilter
-from fuzzywuzzy import fuzz
 
 def relevant_df(keyword):
     def keyword_similarity_advanced(title):
-        if keyword.lower() == title.lower():
-            return 500
+        # Create a TF-IDF vectorizer
+        tfidf_vectorizer = TfidfVectorizer()
+        movie_title = str(title)
+        # Fit and transform the vectorizer on the keyword and movie title
+        tfidf_matrix = tfidf_vectorizer.fit_transform([keyword.lower(), movie_title.lower()])
+        # print(tfidf_matrix)
+        # Calculate the cosine similarity
+        similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0]
 
-        # Calculate the partial ratio between the keyword and the title
-        similarity = fuzz.partial_ratio(keyword.lower(), title.lower())
-
-        return similarity  # Convert to percentage
+        return similarity * 100  # Convert to percentage
 
     re_list = DataFilter(keyword)
     df = re_list[0]
